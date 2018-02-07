f21bbd216c538b16839d943304d83e8f67025d93
Emiline Stewart <estewart0812@gmail.com>
2018-02-07 13:50:07 -0600

    Finished code for codwars kata Credit Card Mask.

---
+++ maskify.py
@@ -0,0 +1,9 @@
+def maskify(cc):
+    cc = str(cc)
+    info = '#' * (len(cc[:-4]))
+    return info + cc[-4:]
+
+c = raw_input('Enter Private Information: ' )
+print maskify(c)
+
+# Hides Private Info, except for the last 4 
