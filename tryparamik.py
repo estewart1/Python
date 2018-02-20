a9a137f067e701fcd39fcbfa0826df1813f68140
Emiline Stewart <estewart0812@gmail.com>
2018-02-20 13:41:48 -0600

    Exploring the use of Paramiko

--- tryparamik.py
+++ tryparamik.py
@@ -1,11 +1,9 @@
 import paramiko
 
-ip = '169.254.169.254'
-username = ''
-password = ''
-
-remote_conn_pre = paramiko.SSHClient()
-remote_conn_pre
-remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False )
+addr = raw_input('What is your ec2 instance address? ')
+username = raw_input('What is your ec2 username? ')
+password = raw_input('What is your password? ')
+remote_conn = paramiko.SSHClient()
+remote_conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+remote_conn.connect(addr, username, password, look_for_keys=False, allow_agent=False, timeout=15)
+ # exploring paramiko 
